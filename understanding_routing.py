bea79d0a0990288c150f2ec6f85f1720aa3e6823
Tomas <tomas.huzevka@outlook.com>
2020-01-21 18:32:12 -0500

    1/21/2020

--- understanding_routing.py
+++ understanding_routing.py
@@ -1,1 +1,39 @@
-# test
+# Create a server file that generates the specified responses for the following url requests:
+
+#     localhost:5000/ - have it say "Hello World!"
+#     localhost:5000/dojo - have it say "Dojo!"
+#     Create one url pattern and function that can handle the following examples:
+#         localhost:5000/say/flask - have it say "Hi Flask!"
+#         localhost:5000/say/michael - have it say "Hi Michael!"
+#         localhost:5000/say/john - have it say "Hi John!"
+#     Create one url pattern and function that can handle the following examples (HINT: int() will come in handy! For example int("35") returns 35):
+#         localhost:5000/repeat/35/hello - have it say "hello" 35 times
+#         localhost:5000/repeat/80/bye - have it say "bye" 80 times
+#         localhost:5000/repeat/99/dogs - have it say "dogs" 99 times
+
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/dojo')
+def dojo():
+    return "Dojo!"
+
+@app.route('/say/<name>')
+def say(name):
+    if type(name) == str:
+        return "Hello, "+ name + "!"
+
+@app.route('/repeat/<number>/<word>')
+def repeat(number, word):
+    return word * int(number)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return 'Sorry! No response. Try again.'    
+
+if __name__=="__main__":
+    app.run(debug=True)
